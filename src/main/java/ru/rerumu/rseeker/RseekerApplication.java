6318340efe9a2de964054171d4eda2151b02f7e7
rybin.gleb <32393520+RybinGlebRG@users.noreply.github.com>
2024-10-09 21:38:28 +0300

    initial

---
+++ src/main/java/ru/rerumu/rseeker/RseekerApplication.java
@@ -0,0 +1,13 @@
+package ru.rerumu.rseeker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RseekerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RseekerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/rerumu/rseeker/RseekerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rerumu.rseeker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RseekerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
